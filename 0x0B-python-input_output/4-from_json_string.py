7d89e3ae1c661e3c16b45976fdb5099676058044
xtar24 <akinfetolulope@gmail.com>
2023-04-12 07:59:17 -0700

    update

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 6-fron_json_string.py
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
