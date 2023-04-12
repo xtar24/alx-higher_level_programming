49f7cc3000af3b7de5ed6b6b922015b6e9190c27
xtar24 <akinfetolulope@gmail.com>
2023-04-12 09:26:09 -0700

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,9 +8,9 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
         Args:
-            first_name (str): The frist nam eof the student.
+            first_name (str): The first name of the student.
             last_name (str): The last name of the student.
-            age (int): The age ofthe student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
