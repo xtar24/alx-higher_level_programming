c27783ccc8e473f2250a0f15a865210a4822cd5e
xtar24 <akinfetolulope@gmail.com>
2023-04-12 01:39:14 -0700

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines an inherited class-checking function."""
 
+
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
+
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
