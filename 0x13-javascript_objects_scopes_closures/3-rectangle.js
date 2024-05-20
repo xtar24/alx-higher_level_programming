feea4cb8790edb4ffbb4564f5e58cf24909f5228
xtar24 <akinfetolulope@gmail.com>
2024-05-20 11:36:31 -0700

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,7 +4,7 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
