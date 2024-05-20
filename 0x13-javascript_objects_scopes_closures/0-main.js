c90b59ed6e1cdddca4331ea1ff8125e669fe4266
xtar24 <akinfetolulope@gmail.com>
2024-05-20 12:06:31 -0700

    update

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -4,5 +4,3 @@ const Rectangle = require('./0-rectangle');
 const r1 = new Rectangle();
 console.log(r1);
 console.log(r1.constructor);
-
-
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-    }
-  print() {
+  }
+
+  print () {
     for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-    }
-  rotate() {
+  }
+
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
-  double() {
+
+  double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
