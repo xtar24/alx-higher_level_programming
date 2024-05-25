4a9880e20bc1566bdb3713e3b3e49204f5600376
xtar24 <akinfetolulope@gmail.com>
2024-05-25 16:21:24 -0700

    update

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities in the database hbtn_0d_usa.
 -- Records are sorted in order of ascending cities.id.
-SELECT c.id, c.name AS CityName, s.name AS StateName
+SELECT c.`id`, c.`name` AS name, s.`name` AS name
  FROM `cities` AS c
 	INNER JOIN `states` AS s
 	ON c.`state_id` = s.`id`
