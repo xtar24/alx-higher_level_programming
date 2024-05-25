52a53688d9d8a9954c5acc746532daa82f7ad75f
xtar24 <akinfetolulope@gmail.com>
2024-05-25 16:03:52 -0700

    update

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
---Lists all cities in the database hbtn_0d_usa.
+-- Lists all cities in the database hbtn_0d_usa.
 -- Records are sorted in order of ascending cities.id.
 SELECT c.`id`, c.`name`, s.`name`
  FROM `cities` AS c
