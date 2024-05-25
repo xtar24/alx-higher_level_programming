35d1d4cb17476ce0f99d9c80985b87d35bb69f38
xtar24 <akinfetolulope@gmail.com>
2024-05-25 15:32:51 -0700

    update

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa with the table cities.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	PRIMARY KEY(`id),
+	PRIMARY KEY(`id`),
 	`id`	INT	NOT NULL AUTO_INCREMENT,
 	`state_id` INT	NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
