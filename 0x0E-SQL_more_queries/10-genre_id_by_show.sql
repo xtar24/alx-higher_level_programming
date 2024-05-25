cdf84f447016d7dda87c9bdc68ebadc05a0c1cdd
xtar24 <akinfetolulope@gmail.com>
2024-05-25 16:00:46 -0700

    update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT s.`title`, g.`genre_id`
  FROM `tv_shows` AS s
 	INNER JOIN `tv_show_genres` AS g
 	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`i;
+ ORDER BY s.`title`, g.`genre_id`;
