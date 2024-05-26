53989f9c7190513028a92ad2defd90dffb46caf8
xtar24 <akinfetolulope@gmail.com>
2024-05-25 17:03:32 -0700

    update

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,5 +4,5 @@
 SELECT s.`title`, g.`genre_id`
  FROM `tv_shows` AS s
 	LEFT JOIN `tv_show_genres` AS g
-	ON s.`id`` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`:
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
